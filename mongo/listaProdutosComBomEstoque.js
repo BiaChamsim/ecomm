e66b246fecaff06ea88d6335fd11005d2fcd44e5
beatriz chamsim <biachamsim@gmail.com>
2023-06-05 22:00:50 -0300

    lista produtos por preco

--- mongo/listaProdutosComBomEstoque.js
+++ mongo/listaProdutosComBomEstoque.js
@@ -2,7 +2,7 @@ use("ecomm");
 
 const produtosComBomEstoque = db.products.find(
     { estoque: {$gte: 3} },
-    { nome: 1, estoque: 1, id: 1}
+    { nome: 1, estoque: 1 }
 )
 
 console.log(produtosComBomEstoque);
